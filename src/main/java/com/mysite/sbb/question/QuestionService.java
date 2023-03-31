ee878d3e09ca6ddbb93b3537dd9056dfd69e78fa
본ìbuffpsigit config --global user.email 깃허브 <rlawlstjd778@naver.com>
2023-03-31 09:53:30 +0900

    Paging function add

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,11 +1,17 @@
 package com.mysite.sbb.question;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import com.mysite.sbb.DataNotFoundException;
 
 
@@ -14,20 +20,23 @@ import com.mysite.sbb.DataNotFoundException;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList(){
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 
-    public Question getQuestion(Integer id){
+    public Question getQuestion(Integer id) {
         Optional<Question> question = this.questionRepository.findById(id);
-        if (question.isPresent()){
+        if (question.isPresent()) {
             return question.get();
-        }else {
+        } else {
             throw new DataNotFoundException("question not found");
         }
     }
 
-    public void create(String  subject, String content){
+    public void create(String subject, String content) {
         Question q = new Question();
         q.setSubject(subject);
         q.setContent(content);
